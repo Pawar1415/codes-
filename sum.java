9b5fb47f994bd3c2f5e4e7df58fa94f39250db5f
Pawar1415 <94797162+Pawar1415@users.noreply.github.com>
2022-09-24 19:26:31 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,12 @@
+
+class sum {
+    public static void main(String[] args) {
+        System.out.println(" Sum Of First 50 Natural Number");
+        
+        int sum  = 0;
+        for(int i=1; i<=50 ; i++)
+            sum += i ;
+            
+        System.out.println(" Total Sum : "+sum);
+    }
+}
